94575dd9e0b642f34cb4d426f754fef7eea58b36
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-25 16:47:00 +0600

    helmet title

--- src/pages/Featured/Featured.jsx
+++ src/pages/Featured/Featured.jsx
@@ -5,7 +5,7 @@ import './Featured.css'
 
 const Featured = () => {
     return (
-        <div className="feature-item  text-white pt-8 my-16">
+        <div className="feature-item  text-white pt-8 my-16 ">
             <SectionTitle subHeading={'check it out'} heading={'Feature Item'}></SectionTitle>
             <div className="flex flex-col md:flex-row items-center py-20 pt-12 px-36">
                 <div >
