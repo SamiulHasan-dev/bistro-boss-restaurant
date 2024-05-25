561ba07c84b4f379c74e96244f32310182b2aa98
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-25 19:54:33 +0600

    custom hook

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,12 +1,18 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../shared/Cover';
+
+import menuImg from '../../assets/menu/banner3.jpg'
+
 
 const Menu = () => {
+    
     return (
         <div>
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
-            <h2>This is menu</h2>
+            <Cover img={menuImg} title={'Our Menu'}></Cover>
+            
         </div>
     );
 };
