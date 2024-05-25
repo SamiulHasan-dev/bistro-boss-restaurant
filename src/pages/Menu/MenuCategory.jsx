2c0a94154d77a4a04910b9fff6cb85d95b930557
SamiulHasan-dev <samiul.199897@gmail.com>
2024-05-26 01:04:08 +0600

    menu catagory

--- src/pages/Menu/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory.jsx
@@ -1,9 +1,16 @@
+import Cover from "../../shared/Cover";
+import MenuItem from "../Shared/MenuItem/MenuItem";
 
 
-const MenuCategory = () => {
+const MenuCategory = ({items, title, img}) => {
     return (
-        <div>
-            
+        <div className="pt-8">
+            {title && <Cover img={img} title={title}></Cover>}
+             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 my-16">
+                {
+                    items.map(item => <MenuItem key={item._id} item={item}></MenuItem> )
+                }
+            </div>
         </div>
     );
 };
