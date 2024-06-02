19ad8a7a013cac3e214c0acf287c1513ae1698cb
SamiulHasan-dev <samiul.199897@gmail.com>
2024-06-02 20:12:42 +0600

    post user email when login with google

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,13 +1,36 @@
 import { FaGoogle } from "react-icons/fa6";
+import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import { useNavigate } from "react-router-dom";
 
 
 const SocialLogin = () => {
+    const {googleSignIn} = useAuth();
+    const axiosPublic = useAxiosPublic();
+    const navigate = useNavigate();
+
+    const handleGoogleSignIn = () =>{
+        googleSignIn()
+        .then(result => {
+            console.log(result.user);
+            const userInfo = {
+                email: result.user?.email,
+                name: result.user?.displayName
+            }
+            axiosPublic.post('/users', userInfo)
+            .then(res => {
+                console.log(res.data)
+                navigate('/');
+            })
+        })
+    }
+
     return (
         <div>
             <div>
-                <button className="btn">
+                <button onClick={handleGoogleSignIn} className="btn bg-primary">
                     <FaGoogle></FaGoogle>
-                    Button
+                    Login With Google
                 </button>
             </div>
         </div>
