a9335c9f4cf9329c48499bba1aa47e057caa42f5
SamiulHasan-dev <samiul.199897@gmail.com>
2024-06-26 23:38:43 +0600

    modify add product

--- src/pages/DashBoard/AddItems.jsx
+++ src/pages/DashBoard/AddItems.jsx
@@ -5,8 +5,6 @@ const AddItems = () => {
     return (
         <div>
             <SectionTitle heading="Add an item" subHeading="Whats New?">
-
-                There is add items section
                 </SectionTitle>
         </div>
     );
